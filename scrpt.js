0b49600968e20004b3a68c55ca4f44ee2360e985
LJKekana <keygley@gmail.com>
2024-04-30 16:05:13 +0200

    another reduce js code

--- scrpt.js
+++ scrpt.js
@@ -2,13 +2,8 @@ $(document).ready(function () {
 
     //variables declaration
     let question1Input = $('#question1_input');
-    let question1Button = $('#question1_button');
-
     let question2Input = $('#question2_input');
-    let question2Button = $('#qustion2_button');
-
     let question3Input = $('#question3_input');
-    let question3Button = $('#qustion3_button');
 
     let input1 = $("#form1");
     let answ1 = $(".answ1");
@@ -29,7 +24,7 @@ $(document).ready(function () {
     $("#randm3").text(randNum);
 
     //event triggers for tabs and generating random numbers
-    $(document).on("click", "#tab1, #refresh,#tab2,refresh2,#tab3,refresh3", function (event) {
+    $(document).on("click", "#tab1, #refresh,#tab2,#refresh2,#tab3,#refresh3", function (event) {
 
         if ($(event.target).is("#tab1") || $(event.target).is("#refresh")) {
             randNum = getRandomNumber(1, 1000);
@@ -55,23 +50,81 @@ $(document).ready(function () {
     /////////////////////////////////////////////////////////////////////////////////////////
     $(document).on("click", "#question1_button, #question2_button,#question3_button", function (event) {
 
-        if ($(event.target).is("#tab1") || $(event.target).is("#refresh")) {
-            randNum = getRandomNumber(1, 1000);
-            $("#randm").text(randNum);
-            answ1.hide();
-            input1.show();
+        console.log(event.target);
+        if ($(event.target).is("#question1_button")) {
+            console.log("1");
+            const inputValue = question1Input.val();
+            //initial variables
+            let sum = 0;
+            let max = randNum;
+
+            for (let i = 1; i < max; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    sum += i;
+                }
+            }
+
+            if (inputValue == sum) {
+
+                swal("Good job!", "Congratulation!", "success");
+                answ1.show();
+                input1.hide();
+                $("#ans1").text(sum);
+
+            } else {
+                console.log("wrong answer");
+                swal("Incorrect!", "Try Again", "error");
+
+            }
         }
-        else if ($(event.target).is("#tab2") || $(event.target).is("#refresh2")) {
-            randNum = getRandomNumber(1, 4000000);
-            $("#randm2").text(randNum);
-            answ2.hide();
-            input2.show();
+        else if ($(event.target).is("#question2_button")) {
+            console.log("2");
+            const inputValue = question2Input.val();
+
+            //initial variables and calculations
+            let number = randNum;
+            let sum = 0, a = 1, b = 2,c;
+            while (b < number) {
+                if (b % 2 == 0) {
+                    sum = sum + b;
+                }
+                c = a + b,a = b, b = c;
+            }
+            if (inputValue == sum) {
+
+                swal("Good job!", "Congratulation", "success");
+                answ2.show();
+                input2.hide();
+                $("#ans2").text(sum);
+
+            } else {
+                swal("Incorrect!", "Try Again", "error");
+            }
+
         }
-        else if ($(event.target).is("#tab3") || $(event.target).is("#refresh3")) {
-            randNum = getRandomNumber(1, 100000);
-            $("#randm3").text(randNum);
-            answ3.hide();
-            input3.show();
+        else if($(event.target).is("#question3_button")) {
+
+            const inputValue = question3Input.val();
+            //initial variables and calculations
+            console.log("3");
+            let number = randNum;
+            let i = 2;
+            while (i < number) {
+                if (number % i == 0) {
+                    number /= i;
+                } else {
+                    i++;
+                }
+            }
+            if (inputValue == number) {
+
+                swal("Good job!", "Congratulation!", "success");
+                answ3.show();
+                input3.hide();
+                $("#ans3").text(number);
+            } else {
+                swal("Incorrect!", "Try Again", "error");
+            }
         }
 
 
@@ -80,87 +133,11 @@ $(document).ready(function () {
 
 
     // Add event listeners to the submit buttons
-    question1Button.on('click', function () {
-        const inputValue = question1Input.val();
 
-
-        //initial variables
-        let sum = 0;
-        let max = randNum;
-
-        for (let i = 1; i < max; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
-            }
-        }
-
-        if (inputValue == sum) {
-
-            swal("Good job!", "Congratulation!", "success");
-            answ1.show();
-            input1.hide();
-            $("#ans1").text(sum);
-
-        } else {
-            console.log("wrong answer");
-            swal("Incorrect!", "Try Again", "error");
-
-        }
-    });
 
     //event listener for question 2 
-    question2Button.on('click', function () {
-        const inputValue = question2Input.val();
 
-        //initial variables and calculations
-        let number = randNum;
-        let sum = 0;
-        let a = 1;
-        let b = 1;
-        let c;
-        while (b < number) {
-            if (b % 2 == 0) {
-                sum = sum + b;
-            }
-            c = a + b;
-            a = b;
-            b = c;
-        }
-
-        if (inputValue == sum) {
-
-            swal("Good job!", "Congratulation", "success");
-            answ2.show();
-            input2.hide();
-            $("#ans2").text(sum);
-
-        } else {
-            swal("Incorrect!", "Try Again", "error");
-        }
-    });
 
     //event listener for button 3
-    question3Button.on('click', function () {
-        const inputValue = question3Input.val();
 
-        //initial variables and calculations
-        let number = randNum;
-        let i = 2;
-        while (i < number) {
-            if (number % i == 0) {
-                number /= i;
-            } else {
-                i++;
-            }
-        }
-        if (inputValue == number) {
-
-            swal("Good job!", "Congratulation!", "success");
-            answ3.show();
-            input3.hide();
-            $("#ans3").text(number);
-        } else {
-            swal("Incorrect!", "Try Again", "error");
-        }
-    });
 });
